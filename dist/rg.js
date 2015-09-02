bf6b82d55d68d3bfcc4e8216544eafdd76e88312
Greg <greg@flashtalking.com>
2015-09-02 13:47:31 +0100

    v1.15.0

--- dist/rg.js
+++ dist/rg.js
@@ -452,6 +452,124 @@ this.text = opts.text || this.width + ' x ' + this.height;
 this.textSize = opts['font-size'] || '30';
 this.format = opts.format || 'png';
 });
+'use strict';
+
+;
+(function () {
+  var _states = [];
+  var findStateByName = function findStateByName(name) {
+    return _states.find(function (state) {
+      if (state.name == name) return state;
+    });
+  };
+  var findStateByUrl = function findStateByUrl(url) {
+    return _states.find(function (state) {
+      if (state.url == url) return state;
+    });
+  };
+  var handlePop = function handlePop(e) {
+    if (e.state) router.go(e.state, true);
+  };
+  var changeState = function changeState(state, popped) {
+    // If supported
+    if (typeof history.pushState != 'undefined' && state.history != false) {
+      // New state
+      if (!history.state || history.state.name != state.name && !popped) {
+        var url = state.url ? '#!/' + state.url : null;
+        history.pushState(state.name, null, url);
+      }
+    }
+    var prevState = Object.assign({}, router.current);
+    router.current = state;
+    router.trigger('go', state, prevState);
+  };
+
+  var router = {
+    add: function add(state) {
+      if (!state || !state.name) {
+        throw 'Please specify a state name';
+        return;
+      }
+      var _state = findStateByName(state);
+      if (_state) _state = state;else _states.push(state);
+      router.trigger('add', _state);
+    },
+
+    remove: function remove(name) {
+      var _state = undefined;
+      _states = _states.filter(function (state) {
+        if (state.name != name) return state;else _state = state;
+      });
+      router.trigger('remove', _state);
+    },
+
+    go: function go(name, popped) {
+      if (!router.active || !name) return;
+      // Match the state in the list of states, if no state available throw error
+      var _state = findStateByName(name);
+      if (!_state) {
+        throw 'State \'' + name + '\' has not been configured';
+        return;
+      }
+
+      // Merge the state options with the parent states
+      var names = name.split('.'); // ["about", "more", "all"]
+      names = names.map(function (name, i) {
+        if (i > 0) {
+          return names.slice(0, i).join('.') + '.' + name;
+        } else {
+          return name;
+        }
+      });
+      names.forEach(function (name, i) {
+        if (i < names.length) {
+          var _parent = findStateByName(name);
+          _state = Object.assign({}, _state, _parent);
+        }
+      });
+
+      // Resolve the resolve function
+      if (typeof _state.resolve == 'function') {
+        var promise = _state.resolve();
+        if (typeof promise.then == 'function') promise.then(function () {
+          return changeState(_state, popped);
+        });
+      } else {
+        changeState(_state, popped);
+      }
+    },
+
+    start: function start() {
+      if (window.location.hash) {
+        var _state = findStateByUrl(window.location.hash.replace('#!/', ''));
+        if (_state) router.go(_state.name);
+      }
+      window.addEventListener('popstate', handlePop);
+      router.active = true;
+      router.trigger('start');
+    },
+
+    stop: function stop() {
+      window.addEventListener('popstate', handlePop);
+      router.active = false;
+      router.trigger('stop');
+    },
+
+    current: undefined,
+    active: true
+  };
+
+  riot.observable(router);
+  riot.mixin('rg.router', {
+    init: function init() {
+      this.router.on('go', this.update);
+    },
+    router: router
+  });
+
+  if (!window.rg) window.rg = {};
+  window.rg.router = router;
+})();
 riot.tag('rg-select', '<div class="container { open: opened }" riot-style="width: { width }"> <input type="text" class="field { open: opened}" onkeydown="{ handleKeys }" onclick="{ toggle }" value="{ fieldText || opts.placeholder }" readonly> <div class="dropdown" show="{ opened }"> <div class="filter"> <input type="text" name="filter" class="filter-box" placeholder="{ opts[\'filter-placeholder\'] || \'Filter\' }" onkeydown="{ handleKeys }" oninput="{ filterItems }"> </div> <div class="list"> <ul> <li each="{ filteredItems }" onclick="{ parent.select }" class="item { selected: selected, disabled: disabled, active: active }"> { text } </li> </ul> </div> </div> </div>', 'rg-select .container, [riot-tag="rg-select"] .container{ position: relative; display: inline-block; cursor: pointer; } rg-select .container.open, [riot-tag="rg-select"] .container.open{ -webkit-box-shadow: 0 2px 10px -4px #444; -moz-box-shadow: 0 2px 10px -4px #444; box-shadow: 0 2px 10px -4px #444; } rg-select .field, [riot-tag="rg-select"] .field{ width: 100%; padding: 10px; background-color: white; border: 1px solid #D3D3D3; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; font-size: 1em; line-height: normal; outline: 0; } rg-select .down-arrow, [riot-tag="rg-select"] .down-arrow{ float: right; } rg-select .dropdown, [riot-tag="rg-select"] .dropdown{ position: relative; width: 100%; background-color: white; border: 1px solid #D3D3D3; border-top: 0; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; } rg-select .container.open .dropdown, [riot-tag="rg-select"] .container.open .dropdown{ -webkit-box-shadow: 0 2px 10px -4px #444; -moz-box-shadow: 0 2px 10px -4px #444; box-shadow: 0 2px 10px -4px #444; } rg-select .filter-box, [riot-tag="rg-select"] .filter-box{ -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; width: 100%; padding: 10px; font-size: 0.9rem; border: 0; outline: none; color: #555; } rg-select ul, [riot-tag="rg-select"] ul,rg-select li, [riot-tag="rg-select"] li{ list-style: none; padding: 0; margin: 0; } rg-select li, [riot-tag="rg-select"] li{ padding: 10px; border-top: 1px solid #E8E8E8; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; } rg-select .selected, [riot-tag="rg-select"] .selected{ font-weight: bold; background-color: #f8f8f8; } rg-select li:hover, [riot-tag="rg-select"] li:hover{ background-color: #f3f3f3; } rg-select li.active, [riot-tag="rg-select"] li.active,rg-select li:hover.active, [riot-tag="rg-select"] li:hover.active{ background-color: #ededed; }', function(opts) {var _this = this;
 
 this.opened = true;
