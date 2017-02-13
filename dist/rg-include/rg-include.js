80832d0de2b475d0364010fb31f5bff80649f023
Anthony Davie <adavie1@users.noreply.github.com>
2017-02-13 16:05:23 +0800

    3.6.0
    Changes from 3.50 - removed the include. prefix the to options. Options are: url: text path to file. unsafe: boolean. If set to true, it will parse the file as HTML regardless of errors.
    
    Example:
    ---HTML:
     <rg-include url="demo.html"></rg-include>
    
    --- SCRIPT:
      var tags = riot.mount('rg-include', {unsafe: true});

--- dist/rg-include/rg-include.js
+++ dist/rg-include/rg-include.js
@@ -1,19 +1,18 @@
-riot.tag2('rg-include', '<div> {responseText} </div>', '', '', function(opts) {
-var _this = this;
-
-var fetch = function fetch() {
-	var req = new XMLHttpRequest();
-	req.onload = function (resp) {
-		if (opts.include.unsafe) _this.root.innerHTML = req.responseText;else _this.responseText = req.responseText;
-		_this.update();
-		_this.trigger('loaded');
-	};
-	req.open('get', opts.include.url, true);
-	req.send();
-	_this.trigger('loading');
-};
-
-this.on('mount', function () {
-	fetch();
-});
+riot.tag("rg-include", "<div> {responseText} </div>", "", "", function(opts) {
+    var _this = this;
+    var fetch = function fetch() {
+        var req = new XMLHttpRequest;
+        req.onload = function(resp) {
+            if (opts.unsafe) _this.root.innerHTML = req.responseText;
+            else _this.responseText = req.responseText;
+            _this.update();
+            _this.trigger("loaded")
+        };
+        req.open("get", opts.url, true);
+        req.send();
+        _this.trigger("loading")
+    };
+    this.on("mount", function() {
+        fetch()
+    })
 });
