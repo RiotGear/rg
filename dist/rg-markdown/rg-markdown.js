8799a4977565cad45ae8911ec72c7a07d8edbf61
Anthony Davie <adavie1@users.noreply.github.com>
2017-02-19 19:01:06 +0800

    3.6.0
    Usage:
    1) <rg-markdown>**testing** content _creation_ again</rg-markdown>
    2) <rg-markdown content="**testing** content _creation_ again x 2"></rg-markdown>
    3) riot.mount('rg-markdown', {markdown: {content: '**Some** _more_ content', url: 'helptext.md'} });
    
    Dependency Note: requires inclusion of commonmark.min.js

--- dist/rg-markdown/rg-markdown.js
+++ dist/rg-markdown/rg-markdown.js
@@ -1,26 +1,46 @@
-riot.tag2('rg-markdown', '', '', '', function(opts) {
-var _this = this;
+riot.tag("rg-markdown", "", "", "", function(opts) {
+    var _this = this ;
 
-if (commonmark) {
-	this.reader = new commonmark.Parser();
-	this.writer = new commonmark.HtmlRenderer();
-}
+    this.on("before-mount", function() {
 
-this.on('update', function () {
-	if (!opts.markdown) opts.markdown = {};
-	if (opts.markdown.content) {
-		_this.root.innerHTML = _this.writer.render(_this.reader.parse(opts.markdown.content));
-	} else if (opts.markdown.url) {
-		(function () {
-			var req = new XMLHttpRequest();
-			req.onload = function (resp) {
-				_this.root.innerHTML = _this.writer.render(_this.reader.parse(req.responseText));
-				_this.trigger('loaded');
-			};
-			req.open('get', opts.markdown.url, true);
-			req.send();
-			_this.trigger('loading');
-		})();
-	}
-});
+      if (!opts.markdown) {
+         opts.markdown = {
+            content: (opts.content ? opts.content : "")
+         }
+      }
+
+      if (opts.content) {
+         opts.markdown.content = opts.content ;
+         }
+
+     if (this.__.innerHTML)
+        opts.markdown.content = this.__.innerHTML ;
+
+
+    }) ;
+
+
+    this.on("mount", function() {
+      if (commonmark) {
+          this.reader = new commonmark.Parser;
+          this.writer = new commonmark.HtmlRenderer
+      }
+
+        if (!opts.markdown) opts.markdown = {};
+        if (opts.markdown.content) {
+            _this.root.innerHTML = _this.writer.render(_this.reader.parse(opts.markdown.content))
+        } else if (opts.markdown.url) {
+            (function() {
+                var req = new XMLHttpRequest;
+                req.onload = function(resp) {
+                    _this.root.innerHTML = _this.writer.render(_this.reader.parse(req.responseText));
+                    _this.trigger("loaded")
+                };
+                req.open("get", opts.markdown.url, true);
+                req.send();
+                _this.trigger("loading")
+            })()
+        }
+    }) ;
+
 });
