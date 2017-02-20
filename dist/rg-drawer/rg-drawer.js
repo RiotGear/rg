60578fa6caf813c640622ea782fae5c5e98bef9f
Anthony Davie <adavie1@users.noreply.github.com>
2017-02-21 03:37:36 +0800

    3.6.0
    Adds functionality for footers etc.
    
    Example usage:
    
    ... HTML:
          <rg-drawer subheading="Something or other" position="bottom">
            The <b>contents</b> of my own special drawer.
          </rg-drawer>
    
         <br / />
         Some random text.
        </div>
    
    --- SCRIPT:
            var tags = riot.mount('rg-drawer', {
              header: "My Drawer Title",
              items : [
                   {text: "items 1"},
                   {text: "Items 2"}
                 ],
              footer:{
                     items : [
                          {text: "Press 1"},
                          {text: "Press 2"}
                        ]
                     }
            });

--- dist/rg-drawer/rg-drawer.js
+++ dist/rg-drawer/rg-drawer.js
@@ -1,18 +1,37 @@
-riot.tag("rg-drawer", '<div class="c-overlay c-overlay--dismissable" onclick="{close}"></div> <div class="o-drawer u-highest {\'o-drawer--\' + opts.position || \'c-drawer--top\'} {\'o-drawer--visible\': opts.isvisible}">  <div class="c-card"> <header class="c-card__header"><h2 class="c-heading c-heading--xsmall">{opts.header}<div class="c-heading__sub">{opts.subheading}</div></h2></header> <ul class="c-card--menu"> <li class="c-card__item {\'c-card__item--active\': active}" each="{opts.items}" onclick="{parent.select}"> {text} </li> </ul> <div class="c-card__body"> <yield></yield> </div></div><div class="c-card__footer"><div class="c-input-group"> <button class="c-button c-button--block" each="{opts.footer.items}" onclick="{parent.select}"> {text} </button> </div></div></div>', "", "", function(opts) {
-    var _this = this;
+riot.tag("rg-drawer",
+    '<div class="c-overlay c-overlay--dismissable" if="{opts.isvisible}" onclick="{close}"></div>' +
+    ' <div class="o-drawer u-highest {\'o-drawer--\' + opts.position || \'c-drawer--top\'} {\'o-drawer--visible\': opts.isvisible}">' +
+    ' <div class="c-card">' +
+    ' <header class="c-card__header">' +
+    '<h2 class="c-heading c-heading--xsmall">{opts.header}' +
+    '<div class="c-heading__sub">{opts.subheading}</div>' +
+    '</h2>' +
+    '</header>' +
+    ' <ul class="c-card--menu"> <li class="c-card__item {\'c-card__item--active\': active}" each="{opts.items}" onclick="{parent.select}"> {text} </li> </ul>' +
+    ' <div class="c-card__body"> <yield></yield> </div>' +
+    '</div>' +
+    '<div class="c-card__footer">' +
+    '<div class="c-input-group"> <button class="c-button c-button--block" each="{opts.footer.items}" onclick="{parent.select}"> {text} </button>' +
+    ' </div></div></div>', "", "",
+    function(opts) {
+        var _this = this;
 
-    this.close = function() {
-       this.unmount() ;
-    };
+        this.on ("before-mount", function(){
+          if (!opts.footer){
+            footer: []
+            }
+        })
 
-    this.select = function(e) {
-        opts.items.forEach(function(item) {
-            return item.active = false
-        });
-        e.item.active = true;
-        _this.trigger("select", e.item)
-    }
-});
+        this.close = function() {
+            opts.isvisible = false ;
+            _this.trigger('close');
+        };
 
-
-
+        this.select = function(e) {
+            opts.items.forEach(function(item) {
+                return item.active = false
+            });
+            e.item.active = true;
+            _this.trigger("select", e.item)
+        }
+    });
