4e7272ea5e5e4cf91c26c4cd5316dffbe217e320
Anthony Davie <adavie1@users.noreply.github.com>
2017-02-22 19:43:00 +0800

    3.6.0
    HTML:
    <rg-pagination page=12 pages=20></rg-pagination>
    
    Script:
    var tags = riot.mount('rg-pagination', {
      pagination: {
        pages: 10,
        page: 6
      }
    })

--- dist/rg-pagination/rg-pagination.js
+++ dist/rg-pagination/rg-pagination.js
@@ -1,37 +1,65 @@
-riot.tag2('rg-pagination', '<div class="pagination"> <div class="pagination__controls pagination__controls--backward"> <button class="pagination__control" __disabled="{opts.pagination.page <= 1}" onclick="{first}">«</button> <button class="pagination__control" __disabled="{opts.pagination.page <= 1}" onclick="{back}">‹</button> </div> <div class="pagination__controls"> <span class="pagination__ellipsis" if="{opts.pagination.page > 2}">&hellip;</span> <button class="pagination__page" onclick="{back}" if="{opts.pagination.page > 1}">{opts.pagination.page - 1}</button> <button class="pagination__page pagination__page--current">{opts.pagination.page}</button> <button class="pagination__page" onclick="{forward}" if="{opts.pagination.page < opts.pagination.pages}">{opts.pagination.page + 1}</button> <span class="pagination__ellipsis" if="{opts.pagination.page < opts.pagination.pages - 1}">&hellip;</span> </div> <div class="pagination__controls pagination__controls--forward"> <button class="pagination__control" __disabled="{opts.pagination.page >= opts.pagination.pages}" onclick="{forward}">›</button> <button class="pagination__control" __disabled="{opts.pagination.page >= opts.pagination.pages}" onclick="{last}">»</button> </div> </div>', '', '', function(opts) {
-var _this = this;
+riot.tag("rg-pagination",
+'<div class="c-pagination">'+
+'     <div class="c-pagination__controls c-pagination__controls--backward">'+
+'       <button class="c-pagination__control" disabled="{opts.pagination.page <= 1}" onclick="{first}">«</button>'+
+'       <button class="c-pagination__control" disabled="{opts.pagination.page <= 1}" onclick="{back}">‹</button>'+
+'     </div>'+
+'     <div class="c-pagination__controls">'+
+'       <span class="c-pagination__ellipsis" if="{opts.pagination.page > 2}">&hellip;</span>'+
+'         <button class="c-pagination__page" onclick="{back}" if="{opts.pagination.page > 1}">{opts.pagination.page - 1}</button>'+
+'         <button class="c-pagination__page c-pagination__page--current">{opts.pagination.page}</button>'+
+'         <button class="c-pagination__page" onclick="{forward}" if="{opts.pagination.page < opts.pagination.pages}">{opts.pagination.page + 1}</button>'+
+'         <span class="c-pagination__ellipsis" if="{opts.pagination.page < opts.pagination.pages - 1}">&hellip;'+
+'        </span>'+
+'       </div>'+
+'     <div class="c-pagination__controls pagination__controls--forward">'+
+'       <button class="c-pagination__control" disabled="{opts.pagination.page >= opts.pagination.pages}" onclick="{forward}">›</button>'+
+'       <button class="c-pagination__control" disabled="{opts.pagination.page >= opts.pagination.pages}" onclick="{last}">»</button>'+
+'     </div>'+
+'</div>'
+ , "", "",
+ function(opts) {
+    var _this = this;
 
-this.on('update', function () {
-  if (!opts.pagination) opts.pagination = {
-    pages: 1,
-    page: 1
-  };
-});
+    this.on("before-mount", function(){
+      if (!opts.pagination){
+          pagination = {
+            pages: 10,
+            page: 1
+            }
+         }
+    if (opts.page)
+       opts.pagination.page = Number(opts.page) ;
 
-this.on('page', function () {
-  var btns = _this.root.querySelectorAll('button');
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].blur();
-  }
-});
+    if (opts.pages)
+       opts.pagination.pages = Number(opts.pages) ;
 
-this.forward = function () {
-  opts.pagination.page++;
-  _this.trigger('page', opts.pagination.page);
-};
+    }) ;
 
-this.back = function () {
-  opts.pagination.page--;
-  _this.trigger('page', opts.pagination.page);
-};
+    this.on("page", function() {
+        var btns = _this.root.querySelectorAll("button");
+        for (var i = 0; i < btns.length; i++) {
+            btns[i].blur()
+        }
+    });
 
-this.first = function () {
-  opts.pagination.page = 1;
-  _this.trigger('page', opts.pagination.page);
-};
+    this.forward = function() {
+        opts.pagination.page++;
+        _this.trigger("page", opts.pagination.page)
+    };
 
-this.last = function () {
-  opts.pagination.page = opts.pagination.pages;
-  _this.trigger('page', opts.pagination.page);
-};
+    this.back = function() {
+        opts.pagination.page--;
+        _this.trigger("page", opts.pagination.page)
+    };
+
+    this.first = function() {
+        opts.pagination.page = 1;
+        _this.trigger("page", opts.pagination.page)
+    };
+
+    this.last = function() {
+        opts.pagination.page = opts.pagination.pages;
+        _this.trigger("page", opts.pagination.page)
+    }
 });
