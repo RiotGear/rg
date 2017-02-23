33025d9d6645cdd3360e59e6a7681b7d8316d8b2
Anthony Davie <adavie1@users.noreply.github.com>
2017-02-23 10:16:09 +0800

    3.6.0
    Uses the webservice http://placeholdit.imigix.net to turn text to images
    
    HTML:
    <rg-placeholdit text="My Text is Better!" background="eee" color="d3c"></rg-placeholdit>
    
    Any of the parameters can be passed either via the script or the html parameters.
    Script:
        var tags = riot.mount('rg-placeholdit', {
          placeholdit: {
            width: 450,
            height: 250,
            background: '000',
            color: 'fff',
            text: 'Hello RiotGear',
            textsize: 30,
            format: 'png'
          }
        })

--- dist/rg-placeholdit/rg-placeholdit.js
+++ dist/rg-placeholdit/rg-placeholdit.js
@@ -1,12 +1,40 @@
-riot.tag2('rg-placeholdit', '<img riot-src="https://placeholdit.imgix.net/~text?bg={opts.placeholdit.background}&txtclr={opts.placeholdit.color}&txt={opts.placeholdit.text}&txtsize={opts.placeholdit.textsize}&w={opts.placeholdit.width}&h={opts.placeholdit.height}&fm={opts.placeholdit.format}">', '', '', function(opts) {
-this.on('update', function () {
-	if (!opts.placeholdit) opts.placeholdit = {};
-	opts.placeholdit.width = opts.placeholdit.width || 450;
-	opts.placeholdit.height = opts.placeholdit.height || 250;
-	opts.placeholdit.background = opts.placeholdit.background || '000';
-	opts.placeholdit.color = opts.placeholdit.color || 'fff';
-	opts.placeholdit.text = opts.placeholdit.text || opts.placeholdit.width + ' x ' + opts.placeholdit.height;
-	opts.placeholdit.textsize = opts.placeholdit.textsize || 30;
-	opts.placeholdit.format = opts.placeholdit.format || 'png';
-});
+riot.tag("rg-placeholdit",
+          '<img riot-src="https://placeholdit.imgix.net/~text?bg={opts.placeholdit.background}'+
+          '&txtclr={opts.placeholdit.color}&txt={opts.placeholdit.text}'+
+          '&txtsize={opts.placeholdit.textsize}&w={opts.placeholdit.width}'+
+          '&h={opts.placeholdit.height}&fm={opts.placeholdit.format}">', "", "",
+          function(opts) {
+
+    this.on("before-mount", function() {
+        if (!opts.placeholdit) opts.placeholdit = {};
+        opts.placeholdit.width = opts.placeholdit.width || 450;
+        opts.placeholdit.height = opts.placeholdit.height || 250;
+        opts.placeholdit.background = opts.placeholdit.background || "000";
+        opts.placeholdit.color = opts.placeholdit.color || "fff";
+        opts.placeholdit.text = opts.placeholdit.text || opts.placeholdit.width + " x " + opts.placeholdit.height;
+        opts.placeholdit.textsize = opts.placeholdit.textsize || 30;
+        opts.placeholdit.format = opts.placeholdit.format || "png"
+
+        if (opts.width)
+           opts.placeholdit.width = Number(opts.width) ;
+
+        if (opts.height)
+          opts.placeholdit.height = Number(opts.height) ;
+
+        if (opts.background)
+           opts.placeholdit.background = opts.background ;
+
+        if (opts.color)
+           opts.placeholdit.color = opts.color ;
+
+        if (opts.text)
+          opts.placeholdit.text = opts.text ;
+
+        if (opts.textsize)
+           opts.placeholdit.textsize = Number(opts.textsize) ;
+
+        if (opts.format)
+           opts.placeholdit.text = opts.format ;
+
+    })
 });
