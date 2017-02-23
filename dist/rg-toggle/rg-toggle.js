d42332e72f4ada0897605eca62cda6fcae64f172
Anthony Davie <adavie1@users.noreply.github.com>
2017-02-23 23:44:54 +0800

    3.6.0
    HTML:
    <rg-toggle></rg-toggle>
    <rg-toggle text="Average White Band"></rg-toggle>
    <rg-toggle  type="info" checked=true></rg-toggle>
    <rg-toggle text="Average White Band" type="success" checked=false></rg-toggle>
    
    Script:
        var tags = riot.mount('rg-toggle', {
          toggle: {
            text: 'My Thang',
            type: 'success',
            checked: false
          }
        })
    
        tags[0].on('toggle', function (checked) { console.log("state", checked) })

--- dist/rg-toggle/rg-toggle.js
+++ dist/rg-toggle/rg-toggle.js
@@ -1,14 +1,46 @@
-riot.tag2('rg-toggle', '<div class="toggle {\'toggle--\' + opts.toggle.type}"> <label class="toggle__wrapper"> <input type="checkbox" __checked="{opts.toggle.checked}" onclick="{toggle}"> <div class="toggle__track"> <div class="toggle__handle"></div> </div> </label> </div>', '', '', function(opts) {
-var _this = this;
+riot.tag("rg-toggle",
+   '<div class="c-toggle {\'c-toggle--\' + opts.toggle.type}">'+
+   '   <label class="c-toggle__wrapper">'+
+   '        <input type="checkbox" checked="{opts.toggle.checked}" onclick="{toggle}">'+
+   '        <div class="c-toggle__track">'+
+   '          <div class="c-toggle__handle"></div>'+
+   '        </div>'+
+   '   </label>'+
+   '   {opts.toggle.text}'+
+   '</div>', ".c-toggle{padding-top: 0.5vh;}", "",
+   function(opts) {
+    var _this = this;
 
-this.on('mount', function () {
-	if (!opts.toggle) opts.toggle = {
-		checked: false
-	};
-});
+    if (!opts.toggle) {
+        opts.toggle = {
+          type: "default"
+        }
+    }
 
-this.toggle = function () {
-	opts.toggle.checked = !opts.toggle.checked;
-	_this.trigger('toggle', opts.toggle.checked);
-};
+    if (opts.text)
+       opts.toggle.text = opts.text ;
+
+    if (opts.type)
+       opts.toggle.type = opts.type ;
+
+       if (opts.checked)
+          if (typeof opts.checked === "string")
+             if (opts.checked.toLowerCase() === "true" || opts.checked.toLowerCase() === "false")
+                if (opts.checked.toLowerCase() ==="true")
+                   opts.toggle.checked = true ;
+                else
+                   opts.toggle.checked = false ;
+
+
+
+    this.on("mount", function() {
+        if (!opts.toggle) opts.toggle = {
+            checked: false
+        }
+    });
+
+    this.toggle = function() {
+        opts.toggle.checked = !opts.toggle.checked;
+        _this.trigger("toggle", opts.toggle.checked)
+    }
 });
