16ac20b4d04f8dc74452fdd1be3a2381b1181da8
Anthony Davie <adavie1@users.noreply.github.com>
2017-02-23 21:19:28 +0800

    3.6.0
    HTML:
    <rg-tabs></rg-tabs>
    <rg-tabs type="info"></rg-tabs>
    
    Script:
        var tags = riot.mount('rg-tabs', {
          tabs: {
          type: 'success',  //default|brand|info|warning|success|error
          tabs: [{
            heading: 'Tab one',
            text: 'This is tab one'
          }, {
            heading: 'Tab two',
            text: 'This is tab two',
            active: true
          }, {
            heading: 'Disabled tab',
            text: 'This is disabled tab',
            disabled: true
          }, {
            heading: 'Tab three',
            include: 'tab.html'
          }]
          }
        })
    
        tags[0].on('open', function (tab) { console.log('open', tab) })
               .on('loading', function (tab) { console.log('loading', tab) })
               .on('loaded', function (tab) { console.log('loaded', tab) })

--- dist/rg-tabs/rg-tabs.js
+++ dist/rg-tabs/rg-tabs.js
@@ -1,36 +1,26 @@
-riot.tag2('rg-tabs', '<div class="tabs {\'tabs--\' + opts.tabs.type}"> <div class="tabs__headings"> <div each="{opts.tabs.tabs}" class="tab-heading {\'tab-heading--active\': active, \'tab-heading--disabled\': disabled}" onclick="{parent.open}"> {heading} </div> </div> <div each="{opts.tabs.tabs}" class="tabs__tab {\'tabs__tab--active\': active}"> <div if="{text}"> {text} </div> <div if="{include}"> {include.responseText} </div> </div> </div>', '', '', function(opts) {
-var _this = this;
+riot.tag("rg-tabs",
+    '<div class="c-tabs {\'c-tabs--\' + opts.tabs.type}">'+
+    '   <div class="c-tabs__headings"> '+
+    '     <div each="{opts.tabs.tabs}" class="c-tab-heading {\'c-tab-heading--active\': active, \'c-tab-heading--disabled\': disabled}"'+
+                   ' onclick="{parent.open}"> {heading}'+
+    '     </div>'+
+    '   </div>'+
+    '   <div each="{opts.tabs.tabs}" class="c-tabs__tab {\'c-tabs__tab--active\': active}">'+
+    '     <div if="{text}"> {text} </div> <div if="{include}"> {include.responseText}'+
+    '     </div>'+
+    '   </div>'+
+    '</div>', "", "",
+    function(opts) {
+    var _this = this;
 
-var fetch = function fetch(tab) {
-	var req = new XMLHttpRequest();
-	req.onload = function (resp) {
-		var activeTab = _this.root.querySelector('.tabs__tab--active');
-		if (activeTab) activeTab.innerHTML = req.responseText;
-		_this.trigger('loaded', tab);
-	};
-	req.open('get', tab.include, true);
-	req.send();
-	_this.trigger('loading', tab);
-};
-
-this.open = function (e) {
-	var tab = e.item;
-	if (!tab.disabled && !tab.active) {
-		opts.tabs.tabs.forEach(function (tab) {
-			tab.active = false;
-		});
-		_this.trigger('open', tab);
-		tab.active = true;
-	}
-};
-
-this.on('update', function () {
-	if (!opts.tabs) opts.tabs = {};
-	if (!Array.isArray(opts.tabs.tabs)) return;
-	opts.tabs.tabs.forEach(function (tab) {
-		if (!tab.disabled && tab.active && tab.include) {
-			fetch(tab);
-		}
-	});
-});
-});
+    var fetch = function fetch(tab) {
+        var req = new XMLHttpRequest;
+        req.onload = function(resp) {
+            var activeTab = _this.root.querySelector(".c-tabs__tab--active");
+            if (activeTab) activeTab.innerHTML = req.responseText;
+            _this.trigger("loaded", tab)
+        };
+        req.open("get", tab.include, true);
+        req.send();
+        _this.trigger("loading", tab)
+    };
