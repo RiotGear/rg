b3519ca1fcc3276365f9dd6eb2fd36d7b770bb09
Anthony Davie <adavie1@users.noreply.github.com>
2017-02-23 23:10:58 +0800

    3.5.9
    Converted to Blaze CSS, it works, but the timing and some of the toasts don't show.
    
    HTML:
    <rg-toasts></rg-toasts>
    
    Script:
        var tags = riot.mount('rg-toasts', {
          toasts: {
            position: 'bottomleft', // 'bottomright|bottomleft|topleft|topright',
            toasts: [{
              type: 'info',
              text: 'Made you look!',
              sticky: true // Turn off timeout
            }, {
              type: 'warning',
              text: 'Careful now...'
            }, {
              type: 'success',
              text: 'You did it!'
            }, {
              type: 'error',
              text: 'Oops!',
              timeout: 3000 // Default to 6000 if not set
            }]
          }
        })
    
        tags[0].on('close', function (toast) { console.log("toasted", toast) })
               .on('select', function (toast) { console.log("selected toast ", toast)  })

--- dist/rg-toast/rg-toasts.js
+++ dist/rg-toast/rg-toasts.js
@@ -1,37 +1,45 @@
-riot.tag2('rg-toasts', '<div if="{opts.toasts.isvisible}" class="toasts {\'toasts--\' + opts.toasts.position}"> <div each="{opts.toasts.toasts}" class="toast {\'toast--\' + type}" if="{isvisible}" onclick="{parent.toastClicked}"> {text} </div> </div>', '', '', function(opts) {
-var _this = this;
+riot.tag("rg-toasts",
+     '<div if="{opts.toasts.isvisible}" class="c-alerts {\'c-alerts--\' + opts.toasts.position}">'+
+     '   <div each="{opts.toasts.toasts}" class="c-alert {\'c-alert--\' + type}" if="{isvisible}" onclick="{parent.toastClicked}"> {text} </div>'+
+     ' </div>', "", "",
+    function(opts) {
+    var _this = this;
 
-this.toastClicked = function (e) {
-	var toast = e.item;
-	window.clearTimeout(toast.timer);
-	toast.isvisible = false;
-	_this.trigger('select', toast);
-};
+    this.toastClicked = function(e) {
+        var toast = e.item;
+        window.clearTimeout(toast.timer);
+        toast.isvisible = false;
+        _this.trigger("select", toast)
+    };
 
-var _uid = 1;
-var uid = function uid() {
-	return _uid++;
-};
+    var _uid = 1;
 
-this.on('update', function () {
-	if (!opts.toasts || !Array.isArray(opts.toasts.toasts)) return;
-	opts.toasts.position = opts.toasts.position || 'bottomright';
-	opts.toasts.toasts.forEach(function (toast) {
-		if (typeof toast.isvisible == 'undefined') toast.isvisible = true;
-		toast.id = toast.id || uid();
-		if (!toast.timer && !toast.sticky) {
-			toast.startTimer = function () {
-				toast.timer = window.setTimeout(function () {
-					toast.isvisible = false;
-					_this.trigger('close', toast);
-					_this.update();
-				}, toast.timeout || 6000);
-			};
-			toast.startTimer();
-		}
-	});
-	opts.toasts.isvisible = opts.toasts.toasts.filter(function (toast) {
-		return toast.isvisible;
-	}).length > 0;
-});
+    var uid = function uid() {
+        return _uid++
+    };
+
+    this.on("mount", function() {
+        if (!opts.toasts || !Array.isArray(opts.toasts.toasts)) return;
+
+        opts.toasts.position = opts.toasts.position || "bottomright";
+
+        opts.toasts.toasts.forEach(function(toast) {
+            if (typeof toast.isvisible == "undefined") toast.isvisible = true;
+            toast.id = toast.id || uid();
+            if (!toast.timer && !toast.sticky) {
+                toast.startTimer = function() {
+                    toast.timer = window.setTimeout(function() {
+                        toast.isvisible = false;
+                        _this.trigger("close", toast);
+                        _this.update()
+                    }, toast.timeout || 6e3)
+                };
+                toast.startTimer()
+            }
+        });
+
+        opts.toasts.isvisible = opts.toasts.toasts.filter(function(toast) {
+            return toast.isvisible
+        }).length > 0
+    })
 });
