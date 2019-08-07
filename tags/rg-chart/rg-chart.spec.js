65f4b794cdff65ce8e553a544e7e8a830ab39e4a
chriscauley <chris@lablackey.com>
2019-08-07 10:01:07 -0400

    test all charts

--- tags/rg-chart/rg-chart.spec.js
+++ tags/rg-chart/rg-chart.spec.js
@@ -1,13 +1,11 @@
-describe('rg-chart', function () {
-  let tag, oncloseSpy
-  const seconds = 500
-  oncloseSpy = sinon.spy()
-  $('body').append('<rg-chart></rg-chart>')
-  //console.log(Object.keys(window._charts))
-  console.log(0)
+// not the most thorough test. It only verifies that the charts in the demo don't throw errors
 
-  it('is mounted', function () {
-    tag = riot.mount("rg-chart",{chart: _charts.linechart})[0]
-    tag.isMounted.should.be.true
+describe('rg-chart', function () {
+  $('body').append('<rg-chart></rg-chart>')
+  Object.keys(window._charts).forEach( key => {
+    it('is mounted', function () {
+      const tag = riot.mount("rg-chart", { chart: window._charts[key] })[0]
+      tag.isMounted.should.be.true
+    })
   })
 })
