4029f3032113f06e248d6605f5c60b4d8a362c30
chriscauley <chris@lablackey.com>
2019-08-14 12:29:51 +0000

    updating README.md and CONTRIBUTING.md with a few more details

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ The open source component library for <a href="http://riotjs.com">Riot</a>.
 <a href="https://www.npmjs.com/package/riotgear"><img src="https://img.shields.io/npm/v/riotgear.svg?style=flat-square"></a>
 <a href="https://www.npmjs.com/package/riotgear"><img src="https://img.shields.io/npm/dm/riotgear.svg?style=flat-square"></a>
 <a href="https://github.com/RiotGear/rg/blob/master/LICENSE"><img src="https://img.shields.io/npm/l/riotgear.svg?style=flat-square"></a>
+
+## Contributing
+
+If you're interested in contributing or just in running the RiotGear repo locally, checkout the [Contribution Guide](CONTRIBUTING.md)
--- demo/app.js
+++ demo/app.js
@@ -2,28 +2,6 @@ import riot from 'riot'
 import "../dependencies/js/iframify.js"
 
 import './demo.tag'
-import "../tags/rg-alerts/rg-alerts"
-import "../tags/rg-bubble/rg-bubble"
-import "../tags/rg-chart/rg-chart"
-import "../tags/rg-code/rg-code"
-import "../tags/rg-credit-card/rg-credit-card-number"
-import "../tags/rg-date/rg-date"
-import "../tags/rg-drawer/rg-drawer"
-import "../tags/rg-ga/rg-ga"
-import "../tags/rg-iframify/rg-iframify"
-import "../tags/rg-include/rg-include"
-import "../tags/rg-map/rg-map"
-import "../tags/rg-markdown/rg-markdown"
-import "../tags/rg-modal/rg-modal"
-import "../tags/rg-pagination/rg-pagination"
-import "../tags/rg-phone-sim/rg-phone-sim"
-import "../tags/rg-placeholdit/rg-placeholdit"
-import "../tags/rg-raw/rg-raw"
-import "../tags/rg-select/rg-select"
-import "../tags/rg-tabs/rg-tabs"
-import "../tags/rg-tags/rg-tags"
-import "../tags/rg-toast/rg-toasts"
-import "../tags/rg-toggle/rg-toggle"
-import "../tags/rg-unsplash/rg-unsplash"
+import '../all'
 
 riot.mount('rg-demo')
